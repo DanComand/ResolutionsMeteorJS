cae35a16f360576cd26afc85b9414063570c99d8
Dan Comand <dcomand@gmail.com>
2015-11-25 23:04:40 -0500

    added form on client for users to submit their resolutions.

--- MeteorResolution.js
+++ MeteorResolution.js
@@ -1,11 +1,25 @@
 Resolutions = new Mongo.Collection('resolutions');
 
-
-
 if (Meteor.isClient) {
   Template.body.helpers({
     resolutions: function() {
       return Resolutions.find()
+    }
+  });
+
+
+  Template.body.events({
+    'submit .new-resolution': function(event) {
+      var title = event.target.title.value;
+
+      Resolutions.insert({
+        title : title,
+        createdAt : new Date(),
+      });
+
+      event.target.title.value = "";
+
+      return false;
     }
   });
 }

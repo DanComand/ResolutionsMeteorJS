c9d2b1a8441eabd12442efb2b9acd8a47cd14d58
Dan Comand <dcomand@gmail.com>
2015-11-26 22:53:23 -0500

    moved code from client to server for more security.

--- MeteorResolution.js
+++ MeteorResolution.js
@@ -19,10 +19,7 @@ if (Meteor.isClient) {
     'submit .new-resolution': function(event) {
       var title = event.target.title.value;
 
-      Resolutions.insert({
-        title : title,
-        createdAt : new Date(),
-      });
+      Meteor.call("addResolution", title);
 
       event.target.title.value = "";
 
@@ -37,11 +34,10 @@ if (Meteor.isClient) {
   Template.resolution.events({
 
     'click .toggle-checked': function() {
-      Resolutions.update(this._id, {$set: {checked: !this.checked}})
+      Meteor.call("updateResolution", this_id, !this.checked);
     },
-
     'click .delete': function() {
-      Resolutions.remove(this._id);
+      Meteor.call("deleteResolution", this._id);
     }
   });
 
@@ -55,3 +51,28 @@ if (Meteor.isServer) {
     // code to run on server at startup
   });
 }
+
+
+Meteor.methods({
+  addResolution: function(title) {
+    Resolutions.insert({
+    title : title,
+    createdAt : new Date()
+    });
+  },
+  updateResolution: function(id, checked) {
+    Resolutions.update(id, {$set: {checked: checked}});
+  },
+  deleteResolution: function(id) {
+    Resolutions.remove(id);
+  }
+});
+
+
+
+
+
+
+
+
+
